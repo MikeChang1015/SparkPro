960770ca70d26b7907e696c21261df36ed2051c6
MikeChang1015 <146709713+MikeChang1015@users.noreply.github.com>
2025-04-25 01:08:01 +0800

    Add files via upload

--- src/control/registerHandler.js
+++ src/control/registerHandler.js
@@ -6,15 +6,14 @@ const { register } = require("../boundary/registerViewer");
 // console.log("registerController:", require("../boundary/registerController"));
 
 const handleRegister = async (req, res) => {
-  const { username, password, confirmedPassword } = req.body;
+	const result = await register(req.body);
 
-  const result = await register(username, password, confirmedPassword);
-
-  if (!result.success) {
-    return res.status(400).send(result.message);
-  }
-
-  res.send(result.message);
+	if (!result.success) 
+	{
+		return res.status(400).send(result.message);
+	}
+	
+	res.send(result.message);
 };
 
 module.exports = { handleRegister };
